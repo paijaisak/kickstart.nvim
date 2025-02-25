6f78ff3b006b49346ca0e712c860522f4f21f430
Paijá <paijaisak@outlook.com>
2025-02-25 19:42:51 +0100

    add clang-format

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -1,6 +1,7 @@
 return {
   { -- Autoformat
     'stevearc/conform.nvim',
+    dependencies = { 'mason.nvim' },
     event = { 'BufWritePre' },
     cmd = { 'ConformInfo' },
     keys = {
@@ -19,7 +20,7 @@ return {
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { cpp = true }
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
           lsp_format_opt = 'never'
@@ -33,11 +34,21 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        c = { 'clang-format' },
+        cpp = { 'clang-format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+      default_format_opts = {
+        lsp_format = 'fallback',
+      },
+      formatters = {
+        clang_format = {
+          prepend_args = { '--style=file:.clang-format' },
+        },
       },
     },
   },
